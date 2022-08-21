2162c3ff8f8c034c39b4f7e2ef6ee25ded780dbc
SimeonVasilev <simvas@abv.bg>
2022-08-21 09:06:52 +0300

    Added Header class

---
+++ BasicWebServer1.Server/HTTP/Header.cs
@@ -0,0 +1,14 @@
+﻿namespace BasicWebServer1.Server.HTTP
+{
+    public class Header
+    {
+        public Header(string name,string value)
+        {
+            Name = name;
+            Value = value;
+        }
+        public string Name { get; init; }
+        public string Value { get; set; }
+
+    }
+}
