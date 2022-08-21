73b96d622490ade738c74bae5def92ba0216ce30
SimeonVasilev <simvas@abv.bg>
2022-08-21 15:08:56 +0300

    .

--- BasicWebServer1.Server/HTTP/Header.cs
+++ BasicWebServer1.Server/HTTP/Header.cs
@@ -1,9 +1,14 @@
-﻿namespace BasicWebServer1.Server.HTTP
+﻿using BasicWebServer1.Server.Common;
+
+namespace BasicWebServer1.Server.HTTP
 {
     public class Header
     {
         public Header(string name,string value)
         {
+            Guard.AgainstNull(name, nameof(name));
+            Guard.AgainstNull(value, nameof(value));
+
             Name = name;
             Value = value;
         }
