9320be3c1b9b9977b1bb1a35d71dd97bd667a0c7
SimeonVasilev <simvas@abv.bg>
2022-08-22 09:55:53 +0300

    Added RedirectResponse

---
+++ BasicWebServer1.Server/Responses/RedirectResponse.cs
@@ -0,0 +1,13 @@
+﻿using BasicWebServer1.Server.HTTP;
+
+namespace BasicWebServer1.Server.Responses
+{
+    public class RedirectResponse : Response
+    {
+        public RedirectResponse(string location) 
+            : base(StatusCode.Found)
+        {
+            this.Headers.Add(Header.Location, location);
+        }
+    }
+}
